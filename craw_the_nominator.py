9223a4ba1bc1f6801707298d629a3f4bdb08f449
Andreas Ekeroot <andeke@gmail.com>
2017-11-22 21:42:18 +0100

    Loads of polish.

--- craw_the_nominator.py
+++ craw_the_nominator.py
@@ -30,25 +30,37 @@ def all_ids(soup):
             yield e.get('data-id')
 
 def clean_text(txt):
-    return ' '.join(x.strip() for x in txt.splitlines())
+    if isinstance(txt, str):
+        return ' '.join(x.strip() for x in str(txt).splitlines())
+    else:
+        return txt
+
+def new_table_row(soup, *args):
+    row = BeautifulSoup.new_tag(soup, name='tr')
+    for a in args:
+        c = BeautifulSoup.new_tag(soup, name='td')
+        if isinstance(a, str):
+            c.string = clean_text(a)
+        else:
+            c.append(a)
+        row.append(c)
+    return row
 
 def main2():
-    soup = BeautifulSoup(requests.get('https://valberedning.sverok.se').content, 'html.parser')
-
-    for id_ in all_ids(soup):
+    img_url_template = 'https://valberedning.sverok.se/nominee_images/get_image/{}/400/400/true'
+    for id_ in all_ids(BeautifulSoup(requests.get('https://valberedning.sverok.se').content, 'html.parser')):
         url = 'https://valberedning.sverok.se/nominees/view/{}'.format(id_)
-        soup = BeautifulSoup(requests.get(url).content, 'html.parser').find('div', class_='nominee-view')
 
         # Tidy städar HTML på ett fantastiskt vis.
+        # Ös in vår nominee view i Tidy och gör roliga grejer med det som kommer ut.
         p = subprocess.run(['tidy', '-iq', '--hide-comments', 'yes',
             '--output-html', 'yes', '--bare', 'yes', '--clean', 'yes',
             '--hide-comments', 'yes', '--show-warnings', 'no',
             '--show-info', 'no', '--show-errors', '0'],\
-                input=str(soup).encode('utf-8'),
+                input=str(BeautifulSoup(requests.get(url).content, 'html.parser').find('div', class_='nominee-view')).encode('utf-8'),
                 stdout=subprocess.PIPE,
                 )
 
-        # Ny soppa.
         soup = BeautifulSoup(p.stdout, 'html.parser')\
                 .find('div', class_='nominee-view')
 
@@ -57,29 +69,12 @@ def main2():
         name = info.h2.text
         table1 = BeautifulSoup.new_tag(soup, name='table')
         for lbl in info.find_all('label'):
-            row = BeautifulSoup.new_tag(soup, name='tr')
-            c1  = BeautifulSoup.new_tag(soup, name='td')
-            c2  = BeautifulSoup.new_tag(soup, name='td')
-            c1.string = lbl.text
             s = lbl.find_next_sibling('span')
-            if s:
-                c2.string = clean_text(s.text)
-            else:
-                c2.string = ''
-            row.append(c1)
-            row.append(c2)
-            table1.append(row)
+            table1.append(new_table_row(soup, lbl.text, s if s else ''))
         table2 = BeautifulSoup.new_tag(soup, name='table')
         for li in info.find('ul', class_='nominations').find_all('li'):
             [title, property] = [x.text for x in li.find_all('span', limit=2)]
-            row = BeautifulSoup.new_tag(soup, name='tr')
-            c1 = BeautifulSoup.new_tag(soup, name='td')
-            c2 = BeautifulSoup.new_tag(soup, name='td')
-            c1.string = title
-            c2.string = property
-            row.append(c1)
-            row.append(c2)
-            table2.append(row)
+            table2.append(new_table_row(soup, title, property))
         info.clear()
         header = BeautifulSoup.new_tag(soup, name='h1')
         header.string = name
@@ -92,30 +87,12 @@ def main2():
         if lw:
             table = BeautifulSoup.new_tag(soup, name='table')
             for t in soup.find_all('div', class_='checkbox'):
-                row = BeautifulSoup.new_tag(soup, name='tr')
-                c1  = BeautifulSoup.new_tag(soup, name='td')
-                c2  = BeautifulSoup.new_tag(soup, name='td')
-                c1.string = ''
-                if t.find('input').get('checked', False):
-                    c1.string = 'X'
-                c2.string = clean_text(t.find('label').text)
-                row.append(c1)
-                row.append(c2)
-                table.append(row)
+                table.append(new_table_row(soup, 'X' if t.find('input').get('checked', False) else '', t.find('label').text))
             lw.replace_with(table)
         for t in soup.find_all('div', class_='input radio'):
             table = BeautifulSoup.new_tag(soup, name='table')
             for i in t.find_all('input'):
-                row = BeautifulSoup.new_tag(soup, name='tr')
-                c1  = BeautifulSoup.new_tag(soup, name='td')
-                c2  = BeautifulSoup.new_tag(soup, name='td')
-                c1.string = ''
-                if i.get('checked', False):
-                    c1.string = 'X'
-                c2.string = clean_text(i.next_sibling.text)
-                row.append(c1)
-                row.append(c2)
-                table.append(row)
+                table.append(new_table_row(soup, 'X' if i.get('checked', False) else '', i.next_sibling.text))
             t.replace_with(table)
 
         # Uppa h3-taggarna en nivå
@@ -136,8 +113,16 @@ def main2():
         for t in chain(soup.find_all('form'), soup.find_all('div')):
             t.unwrap()
 
+        # Ta bort alla class-attribut.
+        for t in soup.find_all():
+            del t['class']
+
         img = soup.find('img').extract()
-        img['src'] = ''.join(('https://valberedning.sverok.se', img['src']))
+        m = re.match(r'^/nominee_images/get_image/(\d+)', img['src'])
+        if m:
+            img['src'] = img_url_template.format(m.group(1))
+        else:
+            img['src'] = 'https://valberedning.sverok.se/img/useravatar.png'
         soup.find('h1').insert_after(img)
 
         del soup['class']
